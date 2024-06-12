3493b226158bcd34a6394d859a23e205295366bc
Luís Fernando <luisfernandoveras13@gmail.com>
2024-06-11 21:25:47 -0300

    commit automtico

---
+++ main.py
@@ -0,0 +1,24 @@
+import pyautogui as pd
+import time
+
+time.sleep(3)
+
+pd.PAUSE = 1.5
+
+pd.write('git init')
+pd.press('enter')
+pd.write('git add .')
+pd.press('enter')
+pd.write("git commit -m 'commit automático'")
+pd.press('enter')
+pd.write('git branch -M main')
+pd.press('enter')
+pd.write('git remote add origin https://github.com/Luisfrva13/envio.git')
+pd.press('enter')
+pd.write('git push -u origin main')
+
+pd.hotkey('ctrl', 't')
+pd.write('https://github.com/Luisfrva13/envio.git')
+pd.press('enter')
+
+
